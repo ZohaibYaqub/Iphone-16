0ac3de6cec737940594b2efd9932f6dee55be42d
ZohaibYaqub <zohaibyaqub11@gmail.com>
2025-07-23 13:15:22 +0500

    Some Miner Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Header from './Components/Header'
 import Hero1 from './Components/Hero1'
 import Hero2 from './Components/Hero2'
 import Hero3 from './Components/Hero3'
-import Hero4 from './Components/hero4'
+
 import Hero5 from './Components/Hero5'
 import ScrollComponent from './Components/scrollcomponent'
 import Hero6 from './Components/Hero6'
+import Hero4 from './Components/Hero4'
 
 
 const App = () => {
